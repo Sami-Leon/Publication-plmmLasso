1347114c78ea8b50e48cf946d06503ff243380d8
Sami-Leon <52415961+Sami-Leon@users.noreply.github.com>
2023-12-19 10:53:18 -0500

    Update example.R

--- Code/example.R
+++ Code/example.R
@@ -1,5 +1,9 @@
+source("CreateSim.R")
+source("modelselection.R")
+source("posi.R")
+source("test.functions.R")
 
-
+# Generating simulated dataset
 data.sim = simulate_group_inter(N = 100, n.mvnorm = 100, grouped = T,
                                 seed = 12, timepoints = 3:5, nonpara.inter = T,
                                 sample_from = seq(0,52,13), cst_ni = F)
@@ -12,12 +16,28 @@ gamma.grid <- c(
   0.000001, 0.0000001, 0.00000001, 0.000000001
 )
 
+# Run model on a grid of hyperparameter and retrieve the model with the best BIC
 fit.plmm <- select.plmm(
-  data = Group1, gamma = gamma.grid, lambda = lambda.grid,
+  data = data.sim[[1]], gamma = gamma.grid, lambda = lambda.grid,
   crit = "BIC", intercept = T, timexgroup = T
 )
 
-posi = de.sparsified.PLMM(simu = Group1, model = test.c)
+# Visualize overall fit and nonlinear functions 
+plot.fit(fit.plmm, data.sim)
 
-test.nonlinear.functions = f.test(Group1, test.c, n = 1000)
+# Get debiased fixed-effects and pvalues
+posi = de.sparsified.PLMM(simu = data.sim[[1]], model = fit.plmm)
 
+# Format post-selection inference output
+df.posi = cbind(posi$beta.hat, posi$bhat, posi$ci, posi$pv)
+colnames(df.posi) = c("Estimate", "Debiased", "CI lower", "CI upper", "p-value")
+head(df.posi)
+
+# Inference for the nonlinear functions
+test.nonlinear.functions = f.test(data.sim[[1]], fit.plmm, n = 1000)
+
+# Test of equality of the functions H0: f1 = f2
+test.nonlinear.functions[[1]]
+
+# Continuous joint confidence bands for the difference between f1 and f2
+test.nonlinear.functions[[3]]
